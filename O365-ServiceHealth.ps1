d9cb69dae9dadc94b08854005d8da90ba799932a
David Pollay <dpollay@pollay.com>
2014-10-07 12:15:30 -0400

    First commit
    Base functionality

---
+++ O365-ServiceHealth.ps1
@@ -0,0 +1,54 @@
+﻿function o365-ServiceHealth{
+
+begin{
+    $htmlfile = $env:HOME + '\o365-ServiceHealth.html'
+    out-file $htmlfile
+    $Header = @"
+<style>
+TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
+TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
+TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
+.odd  { background-color:#ffffff; }
+.even { background-color:#dddddd; }
+TR:Nth-Child(Even) {Background-Color: #dddddd;}
+TR:Hover TD {Background-Color: #C1D5F8;}
+</style>
+<center>
+<title>
+O365 Service Health
+</title>
+"@
+
+#$cred = get-credential
+
+$jsonPayload = (@{userName=$cred.username;password=$cred.GetNetworkCredential().password;} | convertto-json).tostring()
+$cookie = (invoke-restmethod -contenttype "application/json" -method Post -uri "https://api.admin.microsoftonline.com/shdtenantcommunications.svc/Register" -body $jsonPayload).RegistrationCookie
+
+$jsonPayload = (@{lastCookie=$cookie;locale="en-US";preferredEventTypes=@(0,1)} | convertto-json).tostring()
+$notice = (invoke-restmethod -contenttype "application/json" -method Post -uri "https://api.admin.microsoftonline.com/shdtenantcommunications.svc/GetEvents" -body $jsonPayload)
+}
+
+process{ $events = $notice.Events
+
+$ServiceHealth = $events |Sort-Object LastUpdatedTime -Descending | ForEach-Object{
+    $information = $_ | select-object -Property StartTime, Status, ID, Title, Messages
+    foreach($message in $_.Messages){
+        $information.Messages = $message.MessageText
+    $information}
+} | select @{name="Start Time";expression={$information.StartTime}}, `
+    @{name="Status";expression={$information.Status}}, `
+    @{name="ID";expression={$information.ID}}, `
+    @{name="Title";expression={$information.Title}}, `
+    @{name="Messages";expression={[string]$information.Messages}} `
+    | ConvertTo-Html -Fragment -As Table | Out-String
+    
+
+$Report = ConvertTo-Html -Title "Office 365 Service Health" `
+    -Head $header `
+    -Body "$ServiceHealth"
+}
+
+end{$report | out-file $htmlfile ; Invoke-Expression $htmlfile}
+
+}
+o365-ServiceHealth
